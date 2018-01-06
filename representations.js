b4e001144cad97fcbed833e2dea43564871f6f5a
Oliver <oliver@oliverwisler.ch>
2018-01-06 23:13:01 +0100

    separated the class representation of a log object, implemented display of different locators for class represenation.

---
+++ representations.js
@@ -0,0 +1,84 @@
+/**
+ * Created by oliver on 06.01.18.
+ */
+
+    //this is how you create a class in JS
+var LogRepresentation = function(_input){
+        //local vars
+        this.hashByTime = _input;
+        this.hashByLoc = {};
+
+        this.minRX_Loc = 1; //minimum amount of calls per locator
+        this.maxRX_Loc = 2; //maximum amount of calls per locator
+
+
+        this.markerLayer = L.featureGroup();
+
+
+        //init the object
+        this.createHashByLocator();
+        this.setupMarkerLayer();
+
+
+
+};
+
+
+//this is how you create member functions
+LogRepresentation.prototype.createHashByLocator = function(){
+    console.log("Create Locator Hashmap");
+    var locHashmap = {};
+
+    for(t in this.hashByTime){
+        for(r in this.hashByTime[t]){
+            if(this.hashByTime[t][r].hasOwnProperty('locator')){
+                var locator = this.hashByTime[t][r].locator;
+                if( !(locator in locHashmap)){
+                    locHashmap[locator] = [];
+                }
+                locHashmap[locator].push(this.hashByTime[t][r])
+            }
+        }
+    }
+
+    //get the maximum amount of receptions per locator
+    for (l in locHashmap){
+        if(locHashmap[l].length > this.maxRX_Loc){
+            this.maxRX_Loc = locHashmap[l].length;
+        }
+    }
+
+    //console.log(locHashmap);
+    console.log("Processed Locator hashmap, max calls per loc:"+this.maxRX_Loc);
+    this.hashByLoc = locHashmap;
+};
+
+
+LogRepresentation.prototype.setupMarkerLayer = function(){
+    for( loc in this.hashByLoc ){
+
+        //gather some statistics for this locator
+        var callAmount = this.hashByLoc[loc].length;
+        var avgSNR = this.hashByLoc[loc].map(function(a){return a.snr}).reduce(function(a,b){return (a+b)/2});
+
+        //linearly adjust size of marker between 10 and 40
+        var markerSize = 10+ ((callAmount-this.minRX_Loc)/(this.maxRX_Loc-this.minRX_Loc))*30
+
+        //TODO maiddenhead function is not in this file...
+        var marker = L.circleMarker(maidenhead_to_latlon(loc),{color:'#F00', radius: markerSize});
+        marker.bindPopup("Amount of calls: "+callAmount+" <br> AVG SNR:"+avgSNR.toFixed(1));
+        marker.addTo(this.markerLayer);
+
+
+    }
+
+
+
+};
+
+LogRepresentation.prototype.addTo = function(map){
+    this.markerLayer.addTo(map);
+};
+LogRepresentation.prototype.getBounds = function(){
+    return this.markerLayer.getBounds();
+}
--- wsjt-parser.js
+++ wsjt-parser.js
@@ -40,6 +40,7 @@ function decode_from_line(line)
         try
         {
             ret.pos = maidenhead_to_latlon(locator);
+            ret.locator = locator;
         }
         catch(e)
         {
@@ -64,8 +65,8 @@ function maidenhead_to_latlon(string){
     var midLon = parseInt(string[2]);
     var midLat = parseInt(string[3]);
 
-    var lon = -180+(bigLon-65)*20 + midLon*2 +9;
-    var lat = -90+(bigLat-65)*10 + midLat +4.5;
+    var lon = -180+(bigLon-65)*20 + midLon*2 + 1;
+    var lat = -90+(bigLat-65)*10 + midLat +0.5;
 
     //console.log("Extracted: "+lon+","+lat);
 
