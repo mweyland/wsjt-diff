6ba1ff6720f7e06790a7bba5d8adbc2624cff6fa
Oliver <oliver@oliverwisler.ch>
2018-01-07 00:28:20 +0100

    nicer GUI

--- representations.js
+++ representations.js
@@ -85,3 +85,19 @@ LogRepresentation.prototype.addTo = function(map, control){
 LogRepresentation.prototype.getBounds = function(){
     return this.markerLayer.getBounds();
 }
+
+
+//some helper functions so we don't need jQuery yet...
+function hasClass(ele,cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+function addClass(ele,cls) {
+    if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(ele,cls) {
+    if (hasClass(ele,cls)) {
+        var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+        ele.className=ele.className.replace(reg,' ');
+    }
+}
