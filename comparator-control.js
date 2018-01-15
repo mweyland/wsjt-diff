85264260e552344060bf7cc77d26109265ba81ed
Oliver <oliver@oliverwisler.ch>
2018-01-15 23:00:09 +0100

    sadly I have to inform you that I included jQuery to lessen my burden...

--- comparator-control.js
+++ comparator-control.js
@@ -3,14 +3,48 @@ L.Control.ComparatorControl = L.Control.extend({
         //L.DomEvent.on();
 
         var parent = L.DomUtil.create('div');
-        parent.setAttribute("class","comp-control-parent");
+        parent.setAttribute("class","comp-control-parent MenuContainer");
+
+
+        var upContainer = L.DomUtil.create("div");
+        upContainer.setAttribute("class","MenuRow");
 
         this._root = L.DomUtil.create('div');
         this._root.setAttribute("class","btn-group-vertical");
 
-        parent.appendChild(this._root);
+        upContainer.appendChild(this._root);
+
+        var dwnContainer = L.DomUtil.create("div");
+        dwnContainer.setAttribute("class","MenuRow");
+        var loglinToggle = L.DomUtil.create("div");
+        loglinToggle.setAttribute("class", "btn-group btn-group-toggle loglinToggle");
+
+        var logButton = L.DomUtil.create("label");
+        logButton.setAttribute("class","btn btn-sm btn-secondary active toggleBtn");
+        var logInput = L.DomUtil.create("input");
+        logInput.setAttribute("type","checkbox");
+
+        logButton.innerHTML = "dB";
+
+        logButton.appendChild(logInput);
+
+        var linButton = L.DomUtil.create("label");
+        linButton.setAttribute("class","btn btn-sm btn-secondary toggleBtn");
+        var linInput =  L.DomUtil.create("input");
+        linInput.setAttribute("type","checkbox");
+        linButton.innerHTML = "linear";
+
+        linButton.appendChild(linInput);
 
 
+        loglinToggle.appendChild(logButton);
+        loglinToggle.appendChild(linButton);
+        dwnContainer.appendChild(loglinToggle);
+
+
+        parent.appendChild(upContainer);
+        parent.appendChild(dwnContainer);
+        parent.appendChild(dwnContainer);
 
         return parent;
     },
